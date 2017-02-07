531a730b3f0d7a5688b1f321df2c92e79207db1f
Ryan Walsh <ryan@thehatrack.net>
2017-02-07 11:47:10 -0700

    initial idea commit

---
+++ src/App.js
@@ -0,0 +1,95 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+import "./App.css";
+
+import { decrement, increment, redo, undo } from "./ducks/counter";
+
+class App extends Component {
+	render() {
+		const {
+			  decrement
+			, increment
+			, redo
+			, undo
+		} = this.props;
+		const { currentValue, futureValues, previousValues } = this.props.counter;
+
+		return (
+			<div className="app">
+				<section className="counter">
+					<h1 className="counter__current-value">{ currentValue }</h1>
+					<div className="counter__button-wrapper">
+						<button
+							className="counter__button"
+							onClick={ () => increment( 1 ) }
+						>
+							+1
+						</button>
+						<button
+							className="counter__button"
+							onClick={ () => increment( 5 ) }
+						>
+							+5
+						</button>
+						<button
+							className="counter__button"
+							onClick={ () => decrement( 1 ) }
+						>
+							-1
+						</button>
+						<button
+							className="counter__button"
+							onClick={ () => decrement( 5 ) }
+						>
+							-5
+						</button>
+						<br />
+						<button
+							className="counter__button"
+							disabled={ previousValues.length === 0 }
+							onClick={ undo }
+						>
+							Undo
+						</button>
+						<button
+							className="counter__button"
+							disabled={ futureValues.length === 0 }
+							onClick={ redo }
+						>
+							Redo
+						</button>
+					</div>
+				</section>
+				<section className="state">
+					<pre>
+						{ JSON.stringify( this.props.counter, null, 2 ) }
+					</pre>
+				</section>
+			</div>
+		);
+	}
+}
+
+function mapStateToProps( state ) {
+	return { counter: state };
+}
+
+function mapDispatchToProps( dispatch ) {
+	return {
+		  decrement( amount ) {
+			dispatch( decrement( amount ) );
+		}
+		, increment( amount ) {
+			dispatch( increment( amount ) );
+		}
+		, redo() {
+		  	dispatch( redo() );
+		}
+		, undo() {
+		  	dispatch( undo() );
+		}
+	}
+}
+
+export default connect( mapStateToProps, mapDispatchToProps )( App );
