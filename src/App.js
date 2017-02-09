faa94c24f3149e5bdf734733a9d4781edc9459ca
Ryan Walsh <ryan@thehatrack.net>
2017-02-09 15:52:28 -0700

    fix classes for testing

--- src/App.js
+++ src/App.js
@@ -10,39 +10,39 @@ export class App extends Component {
 					<h1 className="counter__current-value">{ 0 }</h1>
 					<div className="counter__button-wrapper">
 						<button
-							className="counter__button"
+							className="counter__button increment-one"
 							onClick={ () => null }
 						>
 							+1
 						</button>
 						<button
-							className="counter__button"
+							className="counter__button increment-five"
 							onClick={ () => null }
 						>
 							+5
 						</button>
 						<button
-							className="counter__button"
+							className="counter__button decrement-one"
 							onClick={ () => null }
 						>
 							-1
 						</button>
 						<button
-							className="counter__button"
+							className="counter__button decrement-five"
 							onClick={ () => null }
 						>
 							-5
 						</button>
 						<br />
 						<button
-							className="counter__button"
+							className="counter__button undo"
 							disabled={ true }
 							onClick={ () => null }
 						>
 							Undo
 						</button>
 						<button
-							className="counter__button"
+							className="counter__button redo"
 							disabled={ true }
 							onClick={ () => null }
 						>
