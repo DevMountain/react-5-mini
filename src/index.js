afe2b63ddc1ed795596161ce33d5fd94ec960ff8
Ryan Walsh <ryan@thehatrack.net>
2017-02-09 10:39:42 -0700

    found a readme bug, branch swap

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 
 import "./index.css";
+
+import store from "./store";
 
 import App from "./App";
 
 ReactDOM.render(
-	<App />
+	<Provider store={ store }>
+		<App />
+	</Provider>
 	, document.getElementById( 'root' )
 );
