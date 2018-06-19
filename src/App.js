522d7d42afbb0e3953048f2df8e6334c4d934580
Steven Isbell <steveni_92@msn.com>
2018-06-18 22:40:21 -0500

    updated solution

--- src/App.js
+++ src/App.js
@@ -1,60 +1,17 @@
-import React, { Component } from "react";
-import "./App.css";
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+
+import store from './store';
+import './App.css';
+
+import Counter from './Counter';
 
 class App extends Component {
   render() {
     return (
-      <div className="app">
-        <section className="counter">
-          <h1 className="counter__current-value">{ 0 }</h1>
-          <div className="counter__button-wrapper">
-            <button
-              className="counter__button increment-one"
-              onClick={ () => null }
-            >
-              +1
-            </button>
-            <button
-              className="counter__button increment-five"
-              onClick={ () => null }
-            >
-              +5
-            </button>
-            <button
-              className="counter__button decrement-one"
-              onClick={ () => null }
-            >
-              -1
-            </button>
-            <button
-              className="counter__button decrement-five"
-              onClick={ () => null }
-            >
-              -5
-            </button>
-            <br />
-            <button
-              className="counter__button undo"
-              disabled={ true }
-              onClick={ () => null }
-            >
-              Undo
-            </button>
-            <button
-              className="counter__button redo"
-              disabled={ true }
-              onClick={ () => null }
-            >
-              Redo
-            </button>
-          </div>
-        </section>
-        <section className="state">
-          <pre>
-            { JSON.stringify( this.props, null, 2 ) }
-          </pre>
-        </section>
-      </div>
+      <Provider store={store}>
+        <Counter />
+      </Provider>
     );
   }
 }
---
+++ src/store.js
@@ -0,0 +1,5 @@
+import { createStore } from 'redux';
+
+import counter from './ducks/counter';
+
+export default createStore(counter);
